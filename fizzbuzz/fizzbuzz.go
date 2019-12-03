fbbc3cc3533cb44bc3c1129d32cde34174967b1c
harrry9349 <harrry9349@gmail.com>
2019-12-03 20:31:50 +0900

    1

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -2,10 +2,10 @@ package main
 
 import(
 	"fmt"
+	"time"
 )
 
-func main(){
-	max := 100
+func fizzbuzzer(max int) {
 	for n := 1; n <= max; n++ {
 		if n % 3 == 0 {
 			if n % 5 == 0 {			
@@ -21,4 +21,12 @@ func main(){
 			}
 		}
 	}
+}
+
+func main(){
+	start := time.Now()
+	max := 100
+	fizzbuzzer(max)
+	end := time.Now()
+	fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
 }	
